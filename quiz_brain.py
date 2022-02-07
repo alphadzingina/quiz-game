33ab698ed19080ddd40752589e2635723ba890dd
alphadzingina <alphaadx6@gmail.com>
2022-02-07 05:10:19 +0100

    display currrent question and get next question

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,10 +1,15 @@
 class QuizBrain:
 
     def __init__(self, q_list):
+        """constructor method"""
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+        """returns True if question_number is less than no of questions, false otherwise"""
+        self.question_number < len(self.question_list)
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
+        self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        
